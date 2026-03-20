96abbc43d56022c5f829d0fd7b0a7096ca42adc5
Dave Pearson <davep@davep.org>
2026-03-20 14:57:07 +0000

    :bookmark: Bump the version number

--- blogmore.el
+++ blogmore.el
@@ -2,7 +2,7 @@
 ;; Copyright 2026 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.4
+;; Version: 1.5
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/blogmoe.el
 ;; Package-Requires: ((emacs "29.1") (end-it "1.20"))
