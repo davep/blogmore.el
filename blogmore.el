be897e538c4184a5276a62548c4c790b38890d7e
Dave Pearson <davep@davep.org>
2026-03-21 09:51:48 +0000

    :sparkles: Add blogmore-insert-tag

--- blogmore.el
+++ blogmore.el
@@ -58,6 +58,11 @@ argument is the date."
   :type 'hook
   :group 'blogmore)
 
+(defcustom blogmore-tag-link-format "/tag/%s/"
+  "Format string for a link to a tag."
+  :type 'string
+  :group 'blogmore)
+
 
 ;;; Code:
 
@@ -254,6 +259,14 @@ frontmatter."
     (save-excursion
       (insert (format "[](/%s.html)" link)))
     (forward-char)))
+
+;;;###autoload
+(defun blogmore-insert-tag (tag)
+  "Insert a link to TAG on my blog."
+  (interactive (blogmore--with "Tag: " (blogmore--current-tags)))
+  (save-excursion
+    (insert (format "[](%s)" (format blogmore-tag-link-format (blogmore--slug tag)))))
+  (forward-char))
 
 (provide 'blogmore)
 
