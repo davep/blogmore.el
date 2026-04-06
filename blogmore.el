202a52bed0431c0f2f7e7b436524c59233ab9f3b
Dave Pearson <davep@davep.org>
2026-04-06 17:00:30 +0100

    :bookmark: v4.0

--- blogmore.el
+++ blogmore.el
@@ -2,7 +2,7 @@
 ;; Copyright 2026 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 3.1
+;; Version: 4.0
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/blogmore.el
 ;; Package-Requires: ((emacs "29.1"))
